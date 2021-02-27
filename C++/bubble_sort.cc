d05e14ab7b4a75b0a36dcd69177c5ad7339c0167
lorenzophys <63981558+lorenzophys@users.noreply.github.com>
2021-02-27 14:14:21 +0100

    Added bubble sort

---
+++ C++/bubble_sort.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <array>
+#include <string>
+
+
+template <typename Iterator>
+void BubbleSort(Iterator start, Iterator end){
+    for(Iterator it=start; it!=end; ++it){
+        for(Iterator jt=start; jt<it; ++jt){
+            if(*it<*jt){
+                std::swap(*it, *jt);
+            }
+        }
+    }
+}
+
+
+template <typename T>
+void Print(T& container){
+    for(const auto& element : container){
+        std::cout << element << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+
+int main(){
+    std::vector<int> vec = {-2, 94, 7, -90, -34, 30, 24, 3, 100, -23};
+    std::array<double, 10> arr = {-2.5, 94.5, 7.5, -90.5, -34.5, 30.5, 24.5, 3.5, 100.5, -23.5};
+    std::string str = "std::string";
+
+    Print(vec);
+    BubbleSort(vec.begin(), vec.end());
+    Print(vec);
+
+    Print(arr);
+    BubbleSort(arr.begin(), arr.end());
+    Print(arr);
+
+    Print(str);
+    BubbleSort(str.begin(), str.end());
+    Print(str);
+}
