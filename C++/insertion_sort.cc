50edf191b70a6012888e1c69f46fa1ae00ee5833
lorenzophys <63981558+lorenzophys@users.noreply.github.com>
2021-02-27 14:14:32 +0100

    Added insertion sort

---
+++ C++/insertion_sort.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <string>
+
+
+template <typename Iterator>
+void InsertionSort(Iterator start, Iterator end){
+    for(Iterator it=start; it!=end; ++it){
+        std::rotate(std::upper_bound(start, it, *it), it, std::next(it));
+    }
+}
+
+
+template <typename T>
+void Print(T& container){
+    for(const auto& element : container){
+        std::cout << element << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+
+int main(){
+    std::vector<int> vec = {-2, 94, 7, -90, -34, 30, 24, 3, 100, -23};
+    std::array<double, 10> arr = {-2.5, 94.5, 7.5, -90.5, -34.5, 30.5, 24.5, 3.5, 100.5, -23.5};
+    std::string str = "std::string";
+
+    Print(vec);
+    InsertionSort(vec.begin(), vec.end());
+    Print(vec);
+
+    Print(arr);
+    InsertionSort(arr.begin(), arr.end());
+    Print(arr);
+
+    Print(str);
+    InsertionSort(str.begin(), str.end());
+    Print(str);
+}
